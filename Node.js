c18f235fc8519c6cc958fa13f67a8714998890b3
Ukrop86 <73402106+Ukrop86@users.noreply.github.com>
2024-09-03 13:58:00 +0300

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,51 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const admin = require('firebase-admin');
-const crypto = require('crypto');
-const app = express();
+const http = require('http');
 
-// Initialize Firebase Admin SDK
-const serviceAccount = require('./referals-b30ae-firebase-adminsdk-6bcfh-fe6480e5de.json');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://referals-b30ae-default-rtdb.firebaseio.com'
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
 
-const db = admin.database();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/auth', async (req, res) => {
-  const data = req.body; // Telegram sends data in POST body
-  const hash = req.query.hash;
-  
-  // Check if the signature is valid
-  const secret = '7475830625:AAGuEX6UxNLwHe-yIgtyq1mtkKdKM25lzJs'; // Replace with your bot's token
-  const generatedHash = crypto.createHmac('sha256', secret).update(new URLSearchParams(data).toString()).digest('hex');
-  
-  if (generatedHash !== hash) {
-    return res.status(400).send('Invalid signature');
-  }
-
-  const userId = data.id;
-  const user = {
-    id: userId,
-    firstName: data.first_name,
-    lastName: data.last_name,
-    username: data.username,
-    photoUrl: data.photo_url
-  };
-
-  try {
-    await db.ref('users/' + userId).set(user);
-    res.send('User data saved');
-  } catch (error) {
-    console.error('Error saving user data: ', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
